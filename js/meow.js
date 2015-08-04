a96c71694c06e543de25737743277ab62d6efe92
elven <elvenisboy@outlook.com>
2015-08-04 08:51:20 +0800

    小小的升级一下

--- js/meow.js
+++ js/meow.js
@@ -5,3 +5,19 @@
 //}else {
 //$(".ul").remove;}
 //})
+$(document).ready(function() {
+
+	$(".edit-content").hide();
+	$("#header-create").click(function() {
+
+
+		$(".meow-list").css("display", "none");
+		$(".edit-content").show();
+		$(".view-content").hide();
+	});
+	$("#show-content").click(function() {
+		$(".meow-list").hide();
+		$(".meow-content").css("display", "inline");
+	});
+
+})
